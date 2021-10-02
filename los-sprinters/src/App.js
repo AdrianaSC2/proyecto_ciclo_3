b3d4430b09b0092d9c4462a1f5621d0679688228
Jorge Augusto Jaramillo <jorgeajaramillo77@gmail.com>
2021-10-02 02:39:41 -0500

    Creando interfaz login en React

---
+++ los-sprinters/src/App.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+export function App() {
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user.email)
+        console.log("estas logueado")
+      } else {
+        console.log("no logueado")
+      }
+    })
+  }, [])
+
+  const signInWithGoogle = () => {
+    firebase.auth().signInWithPopup(provider)
+      .then((result) => {
+        console.log("estoy logeado con google")
+      }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const credential = error.credential
+        console.log(`error en login errorCode:${errorCode}, msg:${errorMessage}`)
+      });
+  }
+
+  const signOut = () => {
+    firebase.auth().signOut();
+  }
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Los Sprinters
+        </Typography>
+
+        { user ? (
+          <div>
+            {user}
+            <Button onClick={signOut} color="inherit">Logout</Button>
+          </div>
+        ) : 
+        (
+        <Button onClick={signInWithGoogle} color="inherit">Login</Button>
+        )}
+      </Toolbar>
+    </AppBar>
+  )
+}
+
--- los-sprinters/src/index.js
+++ los-sprinters/src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { App } from './App';
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCCUKHvCOT1b86q-XEvudyqIFfaAUJ2il4",
+  authDomain: "los-sprinters.firebaseapp.com",
+  projectId: "los-sprinters",
+  storageBucket: "los-sprinters.appspot.com",
+  messagingSenderId: "750115728977",
+  appId: "1:750115728977:web:8e071c48b8d74647c39270",
+  measurementId: "G-ZDNVMS3VQF"
+}
+
+if(!firebase.apps[0]){
+  firebase.initializeApp(firebaseConfig);
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
