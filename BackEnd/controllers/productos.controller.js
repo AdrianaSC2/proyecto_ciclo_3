824e2e057f28d3b7c359ab8a13de91457467242f
Emanuel-L <emanuel.londonor@gmail.com>
2021-10-11 12:43:00 -0500

    Controladores productos -ventas

--- BackEnd/controllers/productos.controller.js
+++ BackEnd/controllers/productos.controller.js
@@ -1,19 +1,81 @@
-const getProductos = (req, res) => {
-    res.json({msg:"Todos los productos"})  
+const ProductSchema = require('../models/producto')
+
+const getProducto = async (req, res) => {
+    if (typeof req.body != 'undefined') {
+        try {
+            let pruduct = await ProductSchema.findById(req.body.id);
+            res.json({ pruducto });
+        }
+        catch (err) {
+            console.log(err);
+        }
+    } else {
+        res.json({ msg: "No se puede obtener el producto sin el id" })
+    }
 }
 
-const createProducto = (req, res) =>{
-
+const getProductos = async (req, res) => {
+    try {
+        let pruducts = await ProductSchema.find();
+        res.json({ pruductos });
+    }
+    catch (err) {
+        console.log(err);
+    }
 }
 
-const updateProducto = (req, res) =>{
-    
+const createProducto = async (req, res) => {
+    if (typeof req.body != 'undefined') {
+        let producto = new ProductSchema(req.body);
+        try {
+            await producto.save();
+            res.json({ msg: 'Se ha creado el producto ' + producto.id });
+        }
+        catch (err) {
+            console.log(err);
+        }
+    } else {
+        res.json({ msg: "No se puede crear el producto, revisar los parametros" })
+    }
 }
 
-const deleteProducto = (req, res) =>{
-    
+const updateProducto = async (req, res) => {
+    if (typeof req.body != 'undefined') {
+        try {
+            await ProductSchema.findOneAndUpdate(
+                { _id: req.body.id },
+                {
+                   valor: req.body.valor,
+                   descripcion: req.body.descripcion,
+                   estado: req.body.estado
+                }
+            );
+            res.json({ msg: 'Se ha actualizado el producto ' + req.body.id })
+        }
+        catch (err) {
+            console.log(err);
+        }
+    } else {
+        res.json({ msg: "No se puede actualizar el producto, revisar los parametros" })
+    }
 }
 
+
+const deleteProducto = async (req, res) => {
+    if (typeof req.body != 'undefined') {
+        try {
+            await ProductSchema.findOneAndRemove(req.body.id);
+            res.json({ msg: 'Se ha eliminado el producto ' + req.body.id });
+        }
+        catch (err) {
+            console.log(err);
+        }
+    } else {
+        res.json({ msg: "No se puede eliminar el producto sin el id" })
+    }
+}
+
+module.exports.getProducto = getProducto;
 module.exports.getProductos = getProductos;
 module.exports.createProducto = createProducto;
 module.exports.updateProducto = updateProducto;
--- BackEnd/controllers/ventas.controller.js
+++ BackEnd/controllers/ventas.controller.js
@@ -1,19 +1,83 @@
-const getVentas = (req, res) => {
- res.json({msg:"Todas las ventas"}) 
+const SaleSchema = require('../models/venta')
+
+const getVenta = async (req, res) => {
+    if (typeof req.body != 'undefined') {
+        try {
+            let venta = await SaleSchema.findById(req.body.id);
+            res.json({ venta });
+        }
+        catch (err) {
+            console.log(err);
+        }
+    } else {
+        res.json({ msg: "No se puede obtener la venta sin el id" })
+    }
 }
 
-const createVenta = (req, res) =>{
-
+const getVentas = async (req, res) => {
+    try {
+        let ventas = await SaleSchema.find();
+        res.json({ ventas });
+    }
+    catch (err) {
+        console.log(err);
+    }
 }
 
-const updateVenta = (req, res) =>{
-    
+const createVenta = async (req, res) => {
+    if (typeof req.body != 'undefined') {
+        let venta = new SaleSchema(req.body);
+        try {
+            await venta.save();
+            res.json({ msg: 'Se ha creado la venta ' + venta.id });
+        }
+        catch (err) {
+            console.log(err);
+        }
+    } else {
+        res.json({ msg: "No se puede crear la venta, revisar los parametros" })
+    }
 }
 
-const deleteVenta = (req, res) =>{
-    
+const updateVenta = async (req, res) => {
+    if (typeof req.body != 'undefined') {
+        try {
+            await SaleSchema.findOneAndUpdate(
+                { _id: req.body.id },
+                {
+                    valor: req.body.valor,
+                    fecha: req.body.fecha,
+                    documentoCliente: req.body.documentoCliente,
+                    nombreCliente: req.body.nombreCliente,
+                    documentoVendedor: req.body.documentoVendedor,
+                  
+                }
+            );
+            res.json({ msg: 'Se ha actualizado la venta ' + req.body.id })
+        }
+        catch (err) {
+            console.log(err);
+        }
+    } else {
+        res.json({ msg: "No se puede actualizar la venta, revisar los parametros" })
+    }
 }
 
+const deleteVenta = async (req, res) => {
+    if (typeof req.body != 'undefined') {
+        try {
+            await SaleSchema.findOneAndRemove(req.body.id);
+            res.json({ msg: 'Se ha eliminado la venta ' + req.body.id });
+        }
+        catch (err) {
+            console.log(err);
+        }
+    } else {
+        res.json({ msg: "No se puede eliminar la venta sin el id" })
+    }
+}
+
+module.exports.getVenta = getVenta;
 module.exports.getVentas = getVentas;
 module.exports.createVenta = createVenta;
 module.exports.updateVenta = updateVenta;
